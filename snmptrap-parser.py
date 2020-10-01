87e86d1dd51c9799d19607db604ab7d6a55c0868
Roy van Baekel <royvb@quanza.net>
2020-10-01 19:05:24 +0200

    - Reformat to comply with PEP8 - Reformat to be more pythonesc - Reformat & rename some variables to increase readability

--- snmptrap-parser.py
+++ snmptrap-parser.py
@@ -1,49 +1,39 @@
 #!/usr/bin/env python
-#All rights reserved Opensource ICT Solutions B.V.
-#Free to redistribute with mention to Opensource ICT Solutions
+# All rights reserved Opensource ICT Solutions B.V.
+# Free to redistribute with mention to Opensource ICT Solutions
 
-####
-#For use with Zabbix
-#Place in directory
-#point snmptrapd.conf to it:
-# disableAuthorization yes
-# traphandle default /usr/bin/python /usr/bin/snmptrap.py
-#And start zabbix stuffs
+# For use with Zabbix
+# Place in directory
+# Update snmptrapd.conf to use this script:
+#  disableAuthorization yes
+#  traphandle default /usr/bin/python <location of this script>
+# And start Zabbix stuffs
 
-import sys
 import re
+import sys
 import time
 
-#Where to write to:
-destination = "/tmp/zbx_traps.tmp"
+
+def main():
+    destination = "/tmp/zbx_traps.tmp"  # File destination
+
+    # Getting Trap
+    trap = sys.stdin.readlines()  # Read from stdin
+    r = "".join(trap)  # Convert LIST to STRING
+
+    # Format the time string
+    formatted_time = str(time.strftime("%H:%M:%S %Y/%m/%d"))
+
+    # Matching on IPaddress
+    source = re.findall(".snmpTrapAddress.0 ([\d.+]+)", r)[0]
+
+    # Building the HEADER so that Zabbix understands it
+    header = "{0} ZBXTRAP  {1}".format(formatted_time, source)
+
+    # Appending it to the log file for Zabbix to pickup
+    with open(destination, "a") as file:
+        file.write("{0}\n{1}".format(header, "".join(trap)))
 
 
-###Getting Trap####
-trap = sys.stdin.readlines() #Receive stuff
-result = "" #Declare empty variable
-r = result.join(trap) # Convert LIST to STRING
-######
-
-
-###Converting again###
-#MESSAGE = ''.join([line.strip() for  line in r])
-######
-
-######
-time = time.strftime("%H:%M:%S %Y/%m/%d")
-time = str(time)
-######
-
-###Matching on IPaddress###
-source = re.findall('.snmpTrapAddress.0 ([\d.+]+)',r)
-######
-
-###Building the HEADER so that Zabbix understands it###
-HEADER = time + " ZBXTRAP  " + ''.join(source)
-######
-
-
-###Appending it to the log file for Zabbix to pickup###
-with open(destination, "a") as file:
-    file.write (HEADER + "\n" + r)
-######
+if __name__ == "__main__":
+    main()
